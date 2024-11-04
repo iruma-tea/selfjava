50f6b2013d281d0618a3b96f4008067d14b16de9
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-11-04 20:35:43 +0900

    Chap03の学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap03/practice/PCondtion.java
@@ -0,0 +1,8 @@
+package to.msn.wings.selfjava.chap03.practice;
+
+public class PCondtion {
+    public static void main(String[] args) {
+        var value = "はじめして";
+        System.out.println(value == null ? "値なし" : value);
+    }
+}
---
+++ selfjava/src/to/msn/wings/selfjava/chap03/practice/Practice3.java
@@ -0,0 +1,10 @@
+package to.msn.wings.selfjava.chap03.practice;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        String str = null;
+        if (str != null && str.endsWith(".java")) {
+            System.out.println("拡張子は.javaです。");
+        }
+    }
+}

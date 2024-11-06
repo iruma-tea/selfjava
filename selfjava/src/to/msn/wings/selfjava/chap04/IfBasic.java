c1e78aaf53f67192067692526728f181f807e1e0
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-11-06 22:18:04 +0900

    リスト4.10まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap04/IfBasic.java
@@ -0,0 +1,12 @@
+package to.msn.wings.selfjava.chap04;
+
+public class IfBasic {
+    public static void main(String[] args) {
+        var i = 10;
+        if (i == 10) {
+            System.out.println("変数iは10です。");
+        } else {
+            System.out.println("変数iは10ではありません。");
+        }
+    }
+}

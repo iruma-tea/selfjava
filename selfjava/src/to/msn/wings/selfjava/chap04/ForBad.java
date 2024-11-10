4af5503a58a7b40780e070224194fd3767320e39
iruma-tea <shusuke.think.pad@gmail.com>
2024-11-10 19:08:44 +0900

    chap04の学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap04/ForBad.java
@@ -0,0 +1,10 @@
+package to.msn.wings.selfjava.chap04;
+
+public class ForBad {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 6; i++) {
+            System.out.println(i + "番目のループです。");
+        }
+    }
+}

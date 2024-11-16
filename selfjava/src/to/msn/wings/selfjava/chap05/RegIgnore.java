aecd287b2866b88d5149edc9b44578da82713b80
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-11-16 17:24:57 +0900

    リスト5.28まで学習完了

--- selfjava/src/to/msn/wings/selfjava/chap05/RegIgnore.java
+++ selfjava/src/to/msn/wings/selfjava/chap05/RegIgnore.java
@@ -5,7 +5,14 @@ import java.util.regex.Pattern;
 public class RegIgnore {
     public static void main(String[] args) {
         var str = "仕事用はwings@example.comです。プライベート用はYAMA@example.comです。";
-        var ptn = Pattern.compile("[a-z0-9.!#$%&'*+/=?^_{|}~-]+@[a-z0-9-]+(\\.[a-z0-9-]+)*", Pattern.CASE_INSENSITIVE);
+        // var ptn = Pattern.compile("[a-z0-9.!#$%&'*+/=?^_{|}~-]+@[a-z0-9-]+(\\.[a-z0-9-]+)*", Pattern.CASE_INSENSITIVE);
+        var ptn = Pattern.compile(
+        """
+        [a-z0-9.!\\#$%&'*+/=?^_{|}~-]+  # local
+        @                               # delimiter
+        [a-z0-9-]+(\\.[a-z0-9-]+)*      # domain
+        """, Pattern.CASE_INSENSITIVE | Pattern.COMMENTS);
+
         var match = ptn.matcher(str);
         while(match.find()) {
             System.out.println(match.group());

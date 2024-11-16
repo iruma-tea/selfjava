83eb9f29c0f6988da20b839c162715a68a06f004
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-11-16 18:45:43 +0900

    リスト5.34まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap05/RegReplaceAll.java
@@ -0,0 +1,8 @@
+package to.msn.wings.selfjava.chap05;
+
+public class RegReplaceAll {
+    public static void main(String[] args) {
+        var str = "サポートサイトはhttps://www.wings.msn.to/です。";
+        System.out.println(str.replaceAll("(?i)http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w ./?%&=_]*)?", "<a href=\"$0\">$0</a>"));
+    }
+}

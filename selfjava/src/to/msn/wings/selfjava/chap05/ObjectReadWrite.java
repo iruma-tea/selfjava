868876c570638498252f2330f6bcf38e4c4ab392
iruma-tea <shusuke.think.pad@gmail.com>
2024-11-26 00:52:54 +0900

    リスト5.55まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap05/ObjectReadWrite.java
@@ -0,0 +1,26 @@
+package to.msn.wings.selfjava.chap05;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ObjectReadWrite {
+    public static void main(String[] args) {
+        final var file = "/home/shusuke/work_java/selfjava/data/article.ser";
+        try(var out = new ObjectOutputStream(new FileOutputStream(file))) {
+            out.writeObject(new Article("Java11の変更点と新しいAPI", "https://codezine.jp/article/corner/751", false));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        try (var in = new ObjectInputStream(new FileInputStream(file))) {
+            var a = (Article)in.readObject();
+            System.out.println(a);
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

cdfe3a0584c000a268c11e94684c18ba5e2e6776
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-12-01 17:54:40 +0900

    リスト5.68まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap05/HttpPost.java
@@ -0,0 +1,9 @@
+package to.msn.wings.selfjava.chap05;
+
+import java.io.IOException;
+
+public class HttpPost {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+    }
+}

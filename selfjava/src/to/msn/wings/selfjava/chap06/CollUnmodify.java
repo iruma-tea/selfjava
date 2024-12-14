4541b7ff534bf82dd5a77078d65f8abc330a5dc1
iruma-tea <shusuke.think.pad@gmail.com>
2024-12-14 16:48:39 +0900

    リスト6-11まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap06/CollUnmodify.java
@@ -0,0 +1,14 @@
+package to.msn.wings.selfjava.chap06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CollUnmodify {
+    public static void main(String[] args) {
+        var data = new ArrayList<String>(Arrays.asList("バラ", "ひまわり", "あさがお"));
+        var udata = Collections.unmodifiableList(data);
+        udata.set(0, "チューリップ");
+        udata.add("さくら");
+    }
+}

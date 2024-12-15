b1dfe6d8fbf7239651bacd85913c80876138cca4
iruma-tea <shusuke.think.pad@gmail.com>
2024-12-15 12:56:44 +0900

    リスト6.22まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap06/MapTreeSort.java
@@ -0,0 +1,15 @@
+package to.msn.wings.selfjava.chap06;
+
+import java.util.TreeMap;
+
+public class MapTreeSort {
+    public static void main(String[] args) {
+        var data = new TreeMap<String, String>(
+            (x, y) -> x.length() - y.length()
+        );
+        data.put("Rose", "バラ");
+        data.put("Sunflower", "ひまわり");
+        data.put("Morning Glory", "あさがお");
+        System.out.println(data);
+    }
+}

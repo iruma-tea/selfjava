330e92cfd8d06228344c1474a758cdbc5f6e3860
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-08-11 22:52:47 +0900

    練習問題7.2まで学習完了

---
+++ selfjava/src/to/msn/wings/selfjava/chap07/constructor/factory/FactoryBasic.java
@@ -0,0 +1,7 @@
+package to.msn.wings.selfjava.chap07.constructor.factory;
+
+public class FactoryBasic {
+    public static void main(String[] args) {
+        var fc = FactoryClass.getInstance();
+    }
+}

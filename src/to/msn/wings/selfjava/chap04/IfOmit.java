1afe1da1e3312aeac85a7187f9bb55a343d46cba
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-09-27 19:46:50 +0900

    リスト4-12まで学習完了

---
+++ src/to/msn/wings/selfjava/chap04/IfOmit.java
@@ -0,0 +1,11 @@
+package to.msn.wings.selfjava.chap04;
+
+public class IfOmit {
+    public static void main(String[] args) {
+        var i = 1;
+        if (i == 0)
+            System.out.println("変数iは1です。");
+        else
+            System.out.println("変数iは1ではありません。");
+    }
+}
---
+++ src/to/msn/wings/selfjava/chap04/SwitchFall.java
@@ -0,0 +1,21 @@
+package to.msn.wings.selfjava.chap04;
+
+public class SwitchFall {
+    public static void main(String[] args) {
+        var drink = "ビール";
+        switch (drink) {
+            case "日本酒":
+            case "ビール":
+            case "ワイン":
+                System.out.println("醸造酒です。");
+                break;
+            case "ブランデー":
+            case "ウィスキー":
+                System.out.println("蒸留酒です。");
+                break;
+            default:
+                System.out.println("？？？");
+                break;
+        }
+    }
+}

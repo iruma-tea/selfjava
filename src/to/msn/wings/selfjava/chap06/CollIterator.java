052d2c7ea3249f17ecafbb8c64a9940001b894c6
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-25 09:03:42 +0900

    リスト6.4まで学習完了

---
+++ src/to/msn/wings/selfjava/chap06/CollIterator.java
@@ -0,0 +1,14 @@
+package to.msn.wings.selfjava.chap06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollIterator {
+    public static void main(String[] args) {
+        var data = new ArrayList<String>(List.of("バラ", "ひまわり", "あさがお"));
+        var itr = data.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
